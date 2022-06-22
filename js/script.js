b2f46427b96d7661643b612c33aa14be04270ce1
BaggyBoi <baggythegamer@gmail.com>
2022-06-22 19:22:58 +0400

    search added

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ if (productsArray.length == 0) {
         let uname = `Apple`
         let price = `${i + 1}`
         let url = "img/apple.png"
-        let product = { name: uname, price: price, img: url }
+        let type = "fruit"
+        let product = { name: uname, price: price, img: url, type: type }
         productsArray.push(product)
     }
 }
@@ -15,26 +16,17 @@ viewAll()
 function viewAll() {
     $("#viewProducts").empty()
     for (let i = 0; i < productsArray.length; i++) {
-        viewProducts(i)
+        viewProducts(i, productsArray[i].name, productsArray[i].price, productsArray[i].img)
     }
 }
-function viewProducts(a) {
-    let product = productsArray[a]
-    let div = document.createElement("div")
-    div.classList.add("product");
-    div.classList.add("visible");
-    div.setAttribute("id", `i${a}`)
-    let img = document.createElement("img")
-    img.setAttribute("src", product.img);
-    img.classList.add("img")
-    let p1 = document.createElement("p")
-    p1.innerText = product.name
-    let p2 = document.createElement("p")
-    p2.innerText = product.price + "$"
-    div.appendChild(img)
-    div.appendChild(p1)
-    div.appendChild(p2)
-    document.querySelector("#viewProducts").appendChild(div)
+function viewProducts(a, b, c, d) {
+    let div =
+        `<div class="product visible" id="i${a}">
+        <img src="${d}" class="img">
+        <p>${b}</p>
+        <p>${c}$</p>
+    </div>`
+    $("#viewProducts").append(div)
 }
 $("#sellDiv").fadeOut(0);
 let arrObj = JSON.stringify(productsArray)
@@ -48,61 +40,142 @@ $("#sellShow").click(function () {
         }
     });
 });
+$("#searchShow").click(function () {
+    $("#searchDiv").toggleClass("shown");
+    $(document).keydown(function (e) {
+        var keycode = (e.keyCode ? e.keyCode : e.which);
+        if (keycode == '27') {
+            $("#searchDiv").removeClass("shown");
+        }
+    });
+});
 $("#goBack").click(function () {
     $("#sellDiv").fadeOut();
 });
 $("#sellForm").submit(function (e) {
-    debugger;
-    e.preventDefault();
-    let fruitName = $("#nameInput").val();
-    let fruitPrice = $("#priceInput").val();
-    let inputFileTag = document.getElementById("imgInput")
-    if (inputFileTag.files && inputFileTag.files[0]) {
-        var reader = new FileReader();
-        reader.onload = imageIsLoaded;
-        reader.readAsDataURL(inputFileTag.files[0]);
-    }
-    function imageIsLoaded(e) {
-        localStorage.setItem("img", e.target.result);
+    let productType = $("#typeInput").val();
+    if (productType != null) {
+        e.preventDefault();
+        let productName = $("#nameInput").val();
+        let productPrice = $("#priceInput").val();
+        let inputFileTag = document.getElementById("imgInput")
+        if (inputFileTag.files && inputFileTag.files[0]) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(inputFileTag.files[0]);
+        }
+        function imageIsLoaded(e) {
+            localStorage.setItem("img", e.target.result);
 
-        let fruitImg = localStorage.getItem("img")
-        localStorage.removeItem("img")
-        let fruit = {
-            name: fruitName,
-            price: fruitPrice,
-            img: fruitImg
-        }
-        productsArray.push(fruit)
-        $("#sellDiv").fadeOut();
-        alert("Fruit Posted!")
-        let arrObj = JSON.stringify(productsArray)
-        localStorage.setItem("productArr", arrObj)
-        viewAll()
-        $("#nameInput").val(null);
-        $("#priceInput").val(null);
-        $("#imgInput").val(null);
-    };
-});
-$("#searchInput").change(function () {
-    searchForFruits()
-});
-function searchForFruits() {
-    let searchedInput = $("#searchInput").val();
-    for (let i = 0; i < productsArray.length; i++) {
-        if (productsArray[i].name == searchedInput) {
-            console.log("name matched")
-            viewProducts(i)
-        }
-        else if (productsArray[i].price == searchedInput) {
-            $("#viewProducts").text("No products found with given search")
-            console.log("price matched")
-        }
-        else if (searchedInput == "") {
+            let productImg = localStorage.getItem("img")
+            setTimeout(() => {
+                localStorage.removeItem("img")
+
+            }, 1000);
+            let product = {
+                name: productName,
+                price: productPrice,
+                img: productImg,
+                type: productType
+            }
+            productsArray.push(product)
+            $("#sellDiv").fadeOut();
+            alert("Product Posted!")
+            let arrObj = JSON.stringify(productsArray)
+            localStorage.setItem("productArr", arrObj)
             viewAll()
-        }
-        else {
-            $("#viewProducts").empty()
-            $("#viewProducts").text("No products found with given search")
+            $("#nameInput").val(null);
+            $("#priceInput").val(null);
+            $("#imgInput").val(null);
+            $("#typeInput").val(null);
+        };
+    }
+    else {
+        alert("Enter your product's type")
+        e.preventDefault()
+    }
+});
+// $("#searchInput").change(function () {
+//     searchForProducts()
+// });
+// function searchForProducts() {
+//     let searchedInput = $("#searchInput").val();
+//     for (let i = 0; i < productsArray.length; i++) {
+//         if (productsArray[i].name == searchedInput) {
+//             console.log("name matched")
+//             viewProducts(i)
+//         }
+//         else if (productsArray[i].price == searchedInput) {
+//             $("#viewProducts").text("No products found with given search")
+//             console.log("price matched")
+//         }
+
+//         else if (productsArray[i].price == searchedInput) {
+//             $("#viewProducts").text("No products found with given search")
+//             console.log("price matched")
+//         }
+//         else if (searchedInput == "") {
+//             viewAll()
+//         }
+//         else {
+//             $("#viewProducts").empty()
+//             $("#viewProducts").text("No products found with given search")
+//         }
+//     }
+// }
+$("#nameSearchForm").submit(function (e) {
+    e.preventDefault();
+    $("#viewProducts").empty();
+    let searchedInput = $("#nameSearch").val().toLowerCase()
+    for (let i = 0; i < productsArray.length; i++) {
+        let productName = productsArray[i].name.toLowerCase()
+        if (productName.indexOf(searchedInput) != -1) {
+            viewProducts(i, productsArray[i].name, productsArray[i].price, productsArray[i].img)
         }
     }
-}
+    $("#typeSearch").val(null)
+    $("#maxPriceSearch").val(null)
+    $("#minPriceSearch").val(null)
+    $("#searchDiv").toggleClass("shown");
+    if ($("#viewProducts").children().length == 0) {
+        $("#viewProducts").text("No products found, please make sure you've typed it all correct!");
+    }
+});
+$("#priceSearchForm").submit(function (e) {
+    e.preventDefault();
+    debugger;
+    $("#viewProducts").empty();
+    let minPrice = parseInt($("#minPriceSearch").val())
+    let maxPrice = parseInt($("#maxPriceSearch").val())
+    for (let i = 0; i < productsArray.length; i++) {
+        let productPrice = parseInt(productsArray[i].price)
+        if (maxPrice <= productPrice && productPrice <= minPrice) {
+            viewProducts(i, productsArray[i].name, productsArray[i].price, productsArray[i].img)
+        }
+    }
+    $("#nameSearch").val(null)
+    $("#typeSearch").val(null)
+    $("#searchDiv").toggleClass("shown");
+    if ($("#viewProducts").children().length == 0) {
+        $("#viewProducts").text("No products found, please make sure you've typed it all correct!");
+    }
+});
+$("#typeSearchForm").submit(function (e) {
+    e.preventDefault();
+    $("#viewProducts").empty();
+    let searchedInput = $("#typeSearch").val().toLowerCase()
+    for (let i = 0; i < productsArray.length; i++) {
+        let productType = productsArray[i].type.toLowerCase()
+        if (productType.indexOf(searchedInput) != -1) {
+            viewProducts(i, productsArray[i].name, productsArray[i].price, productsArray[i].img)
+        }
+    }
+
+    $("#maxPriceSearch").val(null)
+    $("#minPriceSearch").val(null)
+    $("#nameSearch").val(null)
+    $("#searchDiv").toggleClass("shown");
+    if ($("#viewProducts").children().length == 0) {
+        $("#viewProducts").text("No products found, please make sure you've typed it all correct!");
+    }
+});
